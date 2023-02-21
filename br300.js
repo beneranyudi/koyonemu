329ff1ad60225b408453433dacc6e8723b96d443
beneranyudi <125978764+beneranyudi@users.noreply.github.com>
2023-02-21 18:55:58 +0700

    Create br300.js

---
+++ br300.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : '68b6f5756b9087e6e138a15e203f6523',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/68b6f5756b9087e6e138a15e203f6523/invoke.js"></scr' + 'ipt>');
